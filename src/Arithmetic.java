85d5e56adb35182946517ce91ca2686463d388b0
sachin2k01 <thenamesachin@gmail.com>
2023-11-16 21:52:15 +0530

    Addition Operation is added

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Arithmetic
 {
     public static void main(String[] args)
     {
+        Scanner sc=new Scanner(System.in);
         System.out.println("Welcome to Calender");
+        System.out.println("Enter First Number");
+        double n1=sc.nextDouble();
+        System.out.println("Enter Second Number Number");
+        double n2=sc.nextDouble();
+        double res=n1+n2;
+        System.out.println("Sum of Two Number is"+res);
 
     }
 }
