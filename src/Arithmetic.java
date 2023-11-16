d1397bd83c40813d96d4d220091638d51be3f1c6
sachin2k01 <thenamesachin@gmail.com>
2023-11-16 22:13:16 +0530

    Div & mod is Included program is complete

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -10,7 +10,8 @@ public class Arithmetic
         double n1=sc.nextDouble();
         System.out.println("Enter Second Number Number");
         double n2=sc.nextDouble();
-        System.out.println("Enter 1. for Addition\n Enter 2. for Subtraction\n Enter 3. for Multiplication\n");
+        System.out.println("Enter 1. for Addition\nEnter 2. for Subtraction\nEnter 3. for Multiplication\n");
+        System.out.println("Enter 4. for Division\nEnter 5. for Mod");
         System.out.println("Choose an Option");
         int ch=sc.nextInt();
         if(ch==1)
@@ -25,6 +26,15 @@ public class Arithmetic
         {
             double res=n1*n2;
             System.out.println("Multiply of Two Number is "+res);
+        } else if (ch==4)
+        {
+            double res=n1/n2;
+            System.out.println("Division of Two Number is "+res);
+        } else if (ch==5)
+        {
+            double res=n1%n2;
+            System.out.println("Mod of Two Number is "+res);
+
         } else
         {
             System.out.println("Invalid Choice");
