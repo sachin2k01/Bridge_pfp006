b543ffbb4d88654b88a99d1159ba56baf995f096
sachin2k01 <thenamesachin@gmail.com>
2023-11-17 12:42:10 +0530

    Menu operation for calculator

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -6,38 +6,39 @@ public class Arithmetic
     {
         Scanner sc=new Scanner(System.in);
         System.out.println("Welcome to Calender");
-        System.out.println("Enter First Number");
-        double n1=sc.nextDouble();
-        System.out.println("Enter Second Number Number");
-        double n2=sc.nextDouble();
-        System.out.println("Enter 1. for Addition\nEnter 2. for Subtraction\nEnter 3. for Multiplication\n");
-        System.out.println("Enter 4. for Division\nEnter 5. for Mod");
-        System.out.println("Choose an Option");
-        int ch=sc.nextInt();
-        if(ch==1)
-        {
-            double res=n1+n2;
-            System.out.println("Sum of Two Number is "+res);
-        } else if (ch==2)
-        {
-            double res=n1-n2;
-            System.out.println("Sub of Two Number is "+res);
-        } else if (ch==3)
-        {
-            double res=n1*n2;
-            System.out.println("Multiply of Two Number is "+res);
-        } else if (ch==4)
-        {
-            double res=n1/n2;
-            System.out.println("Division of Two Number is "+res);
-        } else if (ch==5)
-        {
-            double res=n1%n2;
-            System.out.println("Mod of Two Number is "+res);
+        do {
+            System.out.println("Enter 1. for Addition\nEnter 2. for Subtraction\nEnter 3. for Multiplication\nEnter 4. for Division\nEnter 5. for Mod\nEnter Other Key to exit");
+            System.out.println("Choose an Option");
+            int ch = sc.nextInt();
+            if(ch==0||ch>=5)
+            {
+                System.exit(0);
+            }
+            System.out.println("Enter First Number");
+            double n1 = sc.nextDouble();
+            System.out.println("Enter Second Number Number");
+            double n2 = sc.nextDouble();
 
-        } else
-        {
-            System.out.println("Invalid Choice");
-        }
+            switch (ch) {
+                case 1:
+                    System.out.println("Addition of two Number: ");
+                    break;
+
+                case 2:
+                    System.out.println("Subtraction of two Number: ");
+                    break;
+                case 3:
+                    System.out.println("Multiply of two Number: ");
+                    break;
+                case 4:
+                    System.out.println("Division of two Number: ");
+                    break;
+                case 5:
+                    System.out.println("Mod of two Number: ");
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }while (true);
     }
 }
